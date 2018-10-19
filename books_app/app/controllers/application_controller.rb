f8f88b16b2c837d61ee6eea772894de8d86b0c3d
tosite0345 <tl091264@gmail.com>
2018-10-19 15:11:33 +0900

    言語を日本語/英語に切り替えられるようにした

--- books_app/app/controllers/application_controller.rb
+++ books_app/app/controllers/application_controller.rb
@@ -1,21 +1,13 @@
 # frozen_string_literal: true
 
-# before_action :set_locale
-
-# def set_locale
-#   I18n.locale = extract_locale_from_tld || I18n.default_locale
-# end
-
-# # トップレベルドメインからロケールを取得する、なければ+nil+を返す
-# # この動作をローカルPCで行なうためには、
-# #   127.0.0.1 application.com
-# #   127.0.0.1 application.it
-# #   127.0.0.1 application.pl
-# # /etc/hosts上のように記述する必要がある
-# def extract_locale_from_tld
-#   parsed_locale = request.host.split(".").last
-#   I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
-# end
-
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 end
