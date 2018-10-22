793086fe74f4dc206beb9c55658adefa4729cd49
tosite0345 <tl091264@gmail.com>
2018-10-22 10:42:13 +0900

    rails-18nパッケージの追加

--- books_app/config/application.rb
+++ books_app/config/application.rb
@@ -15,9 +15,17 @@ module BooksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
 
-    config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}").to_s]
-    config.i18n.default_locale = :ja
-    config.load_defaults 5.2
+    # config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}").to_s]
+    # config.i18n.default_locale = :ja
+    # config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
+
+    #　#　以下の記述を追記する(設定必須)
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
