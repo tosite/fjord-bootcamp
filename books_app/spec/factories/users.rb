bef5b22aa200e225e7c4c7e37d6c7222a352c6cf
tosite0345 <tl091264@gmail.com>
2018-10-23 15:58:58 +0900

    Viewファイルレイアウト崩れ・erb-lintファイル・不要ファイル削除

--- books_app/spec/factories/users.rb
+++ books_app/spec/factories/users.rb
@@ -2,15 +2,7 @@
 
 FactoryBot.define do
   factory :user, class: User do
-    email { "test@example.com" }
+    email    { "test@example.com" }
     password { "password" }
-    # encrypted_password { "password" }
-
-    # t.string "encrypted_password"
-    # t.string "reset_password_token"
-    # t.datetime "reset_password_sent_at"
-    # t.datetime "remember_created_at"
-    # t.datetime "created_at"
-    # t.datetime "updated_at"
   end
 end
