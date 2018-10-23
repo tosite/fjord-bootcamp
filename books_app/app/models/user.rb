086077b28be46fc2c5fe15b3feb0106f2c92203b
tosite0345 <tl091264@gmail.com>
2018-10-23 14:50:28 +0900

    RSpecテスト実装

--- books_app/app/models/user.rb
+++ books_app/app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :books
+  has_many :books, dependent: :destroy
 end
--- books_app/spec/controllers/books_controller_spec.rb
+++ books_app/spec/controllers/books_controller_spec.rb
@@ -3,13 +3,9 @@
 require "rails_helper"
 
 RSpec.describe BooksController, type: :controller do
-  let(:book)   { Book.create(params) }
-  let(:params) { { title: "title", author: "author", user_id: 1 } }
+  let(:book)   { user.books.create(params) }
+  let(:params) { { title: "title", author: "author" } }
   let(:user)   { create(:user) }
-
-  # def login
-  #   login_user user
-  # end
 
   describe "GET #index" do
     context "ログインしていない場合" do
@@ -19,40 +15,115 @@ RSpec.describe BooksController, type: :controller do
       end
     end
     context "ログインしている場合" do
-      it "Indexページが見える" do
+      before do
         login_user user
-        get :index
-        expect(response).to render_template("books/index")
+        get :index, params: {}
+      end
+      it "200ステータスが返ってくる" do
+        expect(response).to have_http_status(:ok)
+      end
+      it "Indexページが見える" do
+        expect(response).to render_template :index
       end
       it "本が表示されている" do
-        login_user user
-        get :index
-        expect(response).to render_template("books/index")
-        # expect(assigns(:books)).to eq book
+        expect(assigns(:books)).to match_array book
       end
     end
   end
+
+  describe "GET #show" do
+    before do
+      login_user user
+      get :show, params: { id: book.id }
+    end
+    it "200ステータスが返ってくる" do
+      expect(response).to have_http_status(:ok)
+    end
+    it "ページが見える" do
+      expect(response).to render_template :show
+    end
+    it "本が表示されている" do
+      expect(assigns(:book)).to eq book
+    end
+  end
+
+  describe "GET #new" do
+    before do
+      login_user user
+      get :new, params: {}
+    end
+    it "200ステータスが返ってくる" do
+      expect(response).to have_http_status(:ok)
+    end
+    it "本が表示されている" do
+      expect(assigns(:book)).to be_a_new Book
+    end
+  end
+
+  describe "GET #edit" do
+    before do
+      login_user user
+      get :edit, params: { id: book.id }
+    end
+    it "200ステータスが返ってくる" do
+      expect(response).to have_http_status(:ok)
+    end
+    it "ページが見える" do
+      expect(response).to render_template :edit
+    end
+    it "本が表示されている" do
+      expect(assigns(:book)).to eq book
+    end
+  end
+
   describe "POST #create" do
-    def book_post(p)
-      post :create, params: { book: p }, session: {}
+    def book_post
+      post :create, params: { book: params }
     end
-    context "ログインしていない場合" do
-      it "ログイン画面にリダイレクトする" do
-        book_post params
-        expect(response).to redirect_to("/users/sign_in")
-      end
+
+    before { login_user user }
+    it "登録できる" do
+      expect { book_post }.to change(Book, :count).by(1)
     end
-    context "ログインしている場合" do
-      before { login_user user }
-      it "登録できる" do
-        book_post params
-        expect { book_post(params) }.to change(Book, :count).by(1)
-      end
-      it "登録したデータに遷移する" do
-        book_post params
-        b = Book.last
-        expect(response).to redirect_to(book_path(b))
-      end
+    it "登録データにリダイレクトする" do
+      book_post
+      b = Book.last
+      expect(response).to redirect_to(book_path(b))
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+    let(:update_attr) { { title: "update title", author: "update author" } }
+    def book_put
+      put :update, params: { id: book.id, book: params.merge!(update_attr) }, session: {}
+    end
+    before do
+      login_user user
+      book_put
+      book.reload
+    end
+    it "更新できる" do
+      expect { book_put }.to change(Book, :count).by(0)
+    end
+    it "更新されている" do
+      expect(book.title).to eq update_attr[:title]
+      expect(book.author).to eq update_attr[:author]
+    end
+    it "更新データにリダイレクトする" do
+      expect(response).to redirect_to(book_path(Book.last))
+    end
+  end
+
+  describe "DELETE #destroy" do
+    def delete_book(id)
+      delete :destroy, params: { id: id }
+    end
+
+    before { login_user user }
+
+    it "削除できる" do
+      b = book
+      expect { delete_book(b.id) }.to change(Book, :count).by(-1)
     end
   end
 end
